38cce77383a0ac6fe5315111091037793c869ea3
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-04-26 19:47:08 +0530

    config files updated

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0x9d473b732a27efa513ede7bdf54eb6df239e8d4e";
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-sepolia.g.alchemy.com/v2/LP7UddX2G-q4_3Zvw6VLFBL_0Rgr3aLn",
   },
 };
